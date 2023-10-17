59fb67b084cd40afa0e5dfcd7c2a5f845402b74a
Kevin Chuang <kevin.chuang@contentsquare.com>
2023-10-17 22:27:36 +0200

    Add libraries dependencies

--- build.sbt
+++ build.sbt
@@ -3,3 +3,8 @@ import BuildHelper.*
 lazy val root = (project in file("."))
   .settings(nameSettings)
   .settings(standardSettings)
+  .settings(
+    libraryDependencies ++=
+      Dependencies.tika ++
+        Dependencies.zio
+  )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,1 +1,15 @@
-object Dependencies {}
+import sbt.*
+
+object Dependencies {
+  private val tikaVersion = "2.9.0"
+  private val zioVersion = "2.0.18"
+
+  val tika: List[ModuleID] = List(
+    "org.apache.tika" % "tika-core" % tikaVersion,
+    "org.apache.tika" % "tika-parsers" % tikaVersion
+  )
+
+  val zio: List[ModuleID] = List(
+    "dev.zio" %% "zio" % zioVersion
+  )
+}
